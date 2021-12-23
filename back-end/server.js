83e495f83bdeaa5aef4884a03ddf542e0c05854d
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-12-23 15:07:57 +0530

    working backend

---
+++ back-end/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require('dotenv')
+const connectDB = require("./connectDB")
+const routes = require("./route/routes");
+
+const app = express();
+dotenv.config()
+connectDB()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+
+// app.get("/api/notes", (req, res) => {
+//     res.send(notes)
+// });
+
+app.use("/api/users",routes)
+
+  
+const PORT = process.env.PORT || 5000
+  app.listen(
+    PORT,
+    console.log(
+      "Server running"
+    )
+  );
+
+  
+// module.exports = { notFound,errorHandler }
+
